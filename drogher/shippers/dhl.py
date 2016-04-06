0173d18e2c88f4b944b3b12df2259fb0d26fee1d
Jason Bittel <jason.bittel@gmail.com>
2016-04-06 11:19:44 -0700

    Add DHL waybill checksum validation

--- drogher/shippers/dhl.py
+++ drogher/shippers/dhl.py
@@ -4,3 +4,8 @@ from .base import Shipper
 class DHL(Shipper):
     barcode_pattern = r'^\d{10}$'
     shipper = 'DHL'
+
+    @property
+    def valid_checksum(self):
+        sequence, check_digit = self.tracking_number[:-1], self.tracking_number[-1]
+        return int(sequence) % 7 == int(check_digit)
