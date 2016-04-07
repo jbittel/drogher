2c502a77ad18d34470e2be89ed1c7a38e6f3799d
Jason Bittel <jason.bittel@gmail.com>
2016-04-07 08:10:27 -0700

    Test barcode function with all shippers

--- tests/test_drogher.py
+++ tests/test_drogher.py
@@ -5,9 +5,25 @@ from drogher.exceptions import InvalidBarcode
 
 
 class TestDrogher:
-    def test_barcode(self):
+    def test_dhl_barcode(self):
+        shipper = drogher.barcode('1656740256')
+        assert shipper.shipper == 'DHL'
+
+    def test_fedex_express_barcode(self):
+        shipper = drogher.barcode('9632001960000000000400152152152158')
+        assert shipper.shipper == 'FedEx'
+
+    def test_fedex_ground_barcode(self):
+        shipper = drogher.barcode('9611019012345612345671')
+        assert shipper.shipper == 'FedEx'
+
+    def test_ups_barcode(self):
         shipper = drogher.barcode('1Z999AA10123456784')
         assert shipper.shipper == 'UPS'
+
+    def test_usps_barcode(self):
+        shipper = drogher.barcode('420221539101026837331000039521')
+        assert shipper.shipper == 'USPS'
 
     def test_invalid_barcode(self):
         with pytest.raises(InvalidBarcode):
