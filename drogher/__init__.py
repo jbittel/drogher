06ec5baaa799836c656f67b083b77197943d97f2
Jason Bittel <jason.bittel@gmail.com>
2016-04-08 08:44:50 -0700

    Allow barcode classes to be optionally specified

--- drogher/__init__.py
+++ drogher/__init__.py
@@ -1,8 +1,10 @@
 from . import shippers
 
 
-def barcode(b):
-    for klass in ['DHL', 'FedExExpress', 'FedExGround96', 'UPS', 'USPSIMpb', 'USPS13']:
+def barcode(b, barcode_classes=None):
+    if barcode_classes is None:
+        barcode_classes = ['DHL', 'FedExExpress', 'FedExGround96', 'UPS', 'USPSIMpb', 'USPS13']
+    for klass in barcode_classes:
         shipper = getattr(shippers, klass)(b)
         if shipper.is_valid:
             return shipper
